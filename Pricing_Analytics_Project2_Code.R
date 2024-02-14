7f07a808085351e539fcc45c20c3d0e948c71dfd
JiyaYUN <142545110+JiyaYUN@users.noreply.github.com>
2024-02-13 21:18:58 -0500

    Add files via upload

---
+++ Pricing_Analytics_Project2_Code.R
@@ -0,0 +1,12 @@
+remove(list = ls())
+
+setwd("D:/Simon.UR/Spring A/MKT440 Pricing Analytics/")
+
+demo <- read.csv("demo_P2.csv")
+kiwi <- read.csv("kiwi_bubbles_P2.csv")
+
+#4. Logit model without segmentation
+
+#5. Logit model with segmentation
+
+#6. Understanding strategic responses
